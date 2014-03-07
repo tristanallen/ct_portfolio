e8ab87d60c79c88b20055a45a66abbd806b43c85
Clarence Lee <clarencedglee@gmail.com>
2014-03-07 10:14:40 +0000

    multi image attachments retreival sql working

--- wordpress/wp-content/themes/furrynoodles/inc/post-multiple-image-attachments.php
+++ wordpress/wp-content/themes/furrynoodles/inc/post-multiple-image-attachments.php
@@ -2,18 +2,19 @@
 
 class Furrynoodles_Multiple_Image_Attachments
 {
+  private $unique_ref      = 'attachment';
   private $meta_box_id     = 'furrynoodles_multiple_image_attachments';
   private $meta_box_title  = 'Images';
   private $post_type       = 'work';
   private $nonce_action    = 'furrynoodles_multiple_image_attachments';
   private $nonce_name      = 'furrynoodles_multiple_image_attachments_save';
-  private $meta_key_prefix = '_attachment';
   private $post_id;
 
   public function __construct()
   {
     //posts_join does not work in admin...
-    add_filter( 'posts_join', array( $this, 'query_add_join_attachments' ) );
+    //add_filter( 'posts_join',   array( $this, 'query_add_join_attachments' ) );
+    //add_filter( 'posts_select', array( $this, 'query_add_select_attachments' ) );
 
     if( is_admin() ){
       add_action( 'admin_head', array( $this, 'on_post_ready' ) );
@@ -23,19 +24,75 @@ class Furrynoodles_Multiple_Image_Attachments
     }
   }
 
+  public function get_meta_key_prefix()
+  {
+    return '_' . $this->unique_ref . '_';
+  }
+
   public function on_post_ready()
   {
     global $post;
     $this->post_id = $post->ID;
   }
 
-  public function query_add_join_attachments( $query )
+  /*
+  private function should_filter_query()
   {
-    if( get_post_type() !== $this->post_type )
-      return $query;
-
-    //todo code in the join...
+    global $wp_query;
+    return is_single() && $wp_query->query_vars['post_type'] == $this->post_type ;
   }
+
+  public function query_add_select_attachments( $select )
+  {
+    if( !$this->should_filter_query() )
+      return $join;
+
+    $select .= str_replace(
+      array( 
+        '[[UREF]]',
+        '[[META_KEY_PREFIX]]' 
+      ),
+      array( 
+        $this->unique_ref,
+        $this->get_meta_key_prefix()
+      ),
+      '
+      [[UREF]]_image.url
+      '
+      );
+
+    return $select;
+  }
+  */
+
+  /*
+  public function query_add_join_attachments( $join )
+  {
+    if( !$this->should_filter_query() )
+      return $join;
+
+    $join .= str_replace(
+      array( 
+        '[[UREF]]',
+        '[[META_KEY_PREFIX]]' 
+      ),
+      array( 
+        $this->unique_ref,
+        $this->get_meta_key_prefix()
+      ),
+      ' 
+      LEFT JOIN wp_postmeta as [[UREF]]_link
+        ON  [[UREF]]_link.post_id = wp_posts.ID
+        AND [[UREF]]_link.meta_key LIKE "[[META_KEY_PREFIX]]%"
+      LEFT JOIN wp_posts as [[UREF]]_image
+        ON  [[UREF]]_link.post_id = [[UREF]]_image.ID
+      ');
+
+    //var_dump( $join );
+
+    return $join;
+  }
+  */
 
   public function enqueue_scripts( $hook )
   {
@@ -95,7 +152,7 @@ class Furrynoodles_Multiple_Image_Attachments
     {
       $image_id = preg_replace( '/[^0-9]/', '', $image_id );
 
-      $meta_key = $this->meta_key_prefix.'_'.$i;
+      $meta_key = $this->get_meta_key_prefix() . $i;
       update_post_meta( $post_id, $meta_key, $image_id );
       array_push( $order, $image_id );
       $i++;
@@ -131,7 +188,8 @@ class Furrynoodles_Multiple_Image_Attachments
     ?>
 <div class="furrynoodles_multiple_image_attachments_existing_image">
   <h3 class="furrynoodles_multiple_image_attachments_image_name"><?php echo basename($attachment['url']) ?></h3>
-  <img src="<?php echo $attachment['url'] ?>" width="100" height="100" />
+  <?php $wp_upload_dir = wp_upload_dir() ?>
+  <img src="<?php echo $wp_upload_dir['baseurl'] . '/' . $attachment['url'] ?>" width="100" height="100" />
   <input type="hidden" name="furrynoodles_multiple_image_attachments_ids[]" value="%IMAGE_ID%" />
 </div> 
     <?php 
@@ -140,13 +198,28 @@ class Furrynoodles_Multiple_Image_Attachments
 
   private function get_attachments()
   {
-    //$sql = "SELECT * FROM `wp_postmeta` WHERE `meta_key` LIKE '_attachment__' ORDER BY `meta_key`";
-    $sql = 'SELECT * FROM `wp_postmeta`
-            LEFT JOIN `wp_posts`
-            ON `meta_value`=`ID`
-            WHERE `meta_key`
-            LIKE "_attachment__"
-            ORDER BY `meta_key`';
+    $sql .= str_replace( 
+      array(
+        '[[POST_ID]]',
+        '[[META_KEY_PREFIX]]'
+      ), 
+      array(
+        $this->post_id,
+        $this->get_meta_key_prefix()
+      ),
+      '
+      SELECT attachment_meta.*
+      FROM wp_posts post
+        LEFT JOIN wp_postmeta meta ON meta.post_id = post.ID
+        AND meta.meta_key LIKE  "[[META_KEY_PREFIX]]%"
+      LEFT JOIN wp_posts attachment ON meta.meta_value = attachment.ID
+      LEFT JOIN wp_postmeta attachment_meta ON attachment_meta.post_id = attachment.ID
+        AND attachment_meta.meta_key = "_wp_attachment_metadata"
+      WHERE post.ID = [[POST_ID]]
+        AND attachment_meta.meta_key IS NOT NULL
+      ORDER BY meta.meta_key
+      '
+    );
 
     global $wpdb;
     $sql_images = $wpdb->get_results( $sql );
@@ -154,23 +227,12 @@ class Furrynoodles_Multiple_Image_Attachments
     $image_array = array();
     foreach ( $sql_images as $image )
     {
+      $image = unserialize( $image->meta_value );
       array_push($image_array, array(
-        'url' => $image->guid,
-        'id' => $image->meta_value));
+        'url' => dirname( $image['file'] ) . '/' . $image['sizes']['thumbnail']['file'],
+        'id'  => $image->meta_value));
     }
     return $image_array;
-/*
-    return array(
-      array( 'url' => 'fuckayou',
-             'id'  => '21' 
-      ),
-      array( 'url' => '',
-             'id'  => '23'
-      ),
-      array( 'url' => '',
-             'id'  => '26'
-      )
-    );*/
   }
 }
 

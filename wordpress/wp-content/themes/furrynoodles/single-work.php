1608c543197dea36e9a3300cd9d85958b28b9f24
Clarence Lee <clarencedglee@gmail.com>
2014-03-07 11:39:30 +0000

    style tweaks: verdana body font, remove site border-top

--- wordpress/wp-content/themes/furrynoodles/single-work.php
+++ wordpress/wp-content/themes/furrynoodles/single-work.php
@@ -32,7 +32,7 @@ get_header(); ?>
 					<h2><?php the_title(); ?></h2>
 					<div class="work-client">for <?php echo get_post_meta( get_the_ID(), '_work_details_client', true ) ?></div>
           <div class="excerpt">
-					<div class="work-excerpt"><?php the_excerpt(  ) ?></div>
+					<div class="work-excerpt body-font"><?php the_excerpt(  ) ?></div>
           </div>
 
 					<?php else : ?>

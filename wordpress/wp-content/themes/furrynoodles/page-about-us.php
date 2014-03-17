782423b4fcd33fb9e055c8d938963eb8b84f8286
tristanallen <furryfool@gmail.com>
2014-03-17 14:30:19 +0000

    merge

--- wordpress/wp-content/themes/furrynoodles/page-about-us.php
+++ wordpress/wp-content/themes/furrynoodles/page-about-us.php
@@ -10,45 +10,47 @@
  */
 get_header(); ?>
 
-<div class="about-us">
-  <div class="introduction">
-    <h1>Get your digital juices flowing.</h1>
-    <img src="../wp-content/themes/furrynoodles/img/about_us_photo_olde2.jpg" />
-  </div>
-  <div class="people clear">
-    <div class="person">
-      <h3>Clarence Lee</h3>
-      <p>Clarence has managed digital teams at Time Out, led UX projects at Tobias & Tobias, and had illustrations published on the Playstation website.</p>
-    </div>
-    <div class="person">
-      <h3>Tristan Allen</h3>
-      <p>Tristan has worked for Global Interactive Marketing Online, HMX with such clients as Sony, Lenovo, Dell, Carphone Warehouse, panasonic, Nikon</p>
-    </div>
-    <div class="skills">
-      <ul>
-        <li>Visual Design</li>
-        <li>User Experience</li>
-        <li>HTML / CSS</li>
-        <li>PHP / SQL</li>
-        <li>iOS</li>
-      </ul>
-      <ul>
-        <li>Development</li>
-        <li>Flash</li>
-        <li>HTML / CSS</li>
-        <li>PHP / SQL</li>
-        <li>Android</li>
-      </ul>
-    </div>
-  </div>
-  <div class="services-skills clear">
-    <ul>
-      <li>Websites</li>
-      <li>Webapps</li>
-      <li>Mobile Apps (iOS/Android)</li>
-      <li>HTML Emails</li>
-    </ul>
-  </div>
+<div class="introduction">
+  <p>Get your digital juices flowing.</p>
+    <!--img src="../wp-content/themes/furrynoodles/img/about_us_photo_olde2.jpg" /-->
 </div>
+<section>
+  <div class="about-us">
+    <div class="people clear">
+      <div class="person">
+        <h3>Clarence Lee</h3>
+        <p>Clarence has managed digital teams at Time Out, led UX projects at Tobias & Tobias, and had illustrations published on the Playstation website.</p>
+      </div>
+      <div class="person">
+        <h3>Tristan Allen</h3>
+        <p>Tristan has worked for Global Interactive Marketing Online, HMX with such clients as Sony, Lenovo, Dell, Carphone Warehouse, panasonic, Nikon</p>
+      </div>
+      <div class="skills">
+        <ul>
+          <li>Visual Design</li>
+          <li>User Experience</li>
+          <li>HTML / CSS</li>
+          <li>PHP / SQL</li>
+          <li>iOS</li>
+        </ul>
+        <ul>
+          <li>Development</li>
+          <li>Flash</li>
+          <li>HTML / CSS</li>
+          <li>PHP / SQL</li>
+          <li>Android</li>
+        </ul>
+      </div>
+    </div>
+    <div class="services-skills clear">
+      <ul>
+        <li>Websites</li>
+        <li>Webapps</li>
+        <li>Mobile Apps (iOS/Android)</li>
+        <li>HTML Emails</li>
+      </ul>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
